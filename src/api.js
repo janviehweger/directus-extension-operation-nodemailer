a95140b570d32f66d6eefa29a73468e538cc59e1
Jan Viehweger <viehweger@visual.io>
2025-05-13 05:37:09 +0200

    added fields

--- src/api.js
+++ src/api.js
@@ -1,6 +1,55 @@
-export default {
-	id: 'custom',
-	handler: ({ text }) => {
-		console.log(text);
-	},
-};
+import nodemailer from "nodemailer";
+import { defineOperationApi } from "@directus/extensions-sdk";
+
+export default defineOperationApi({
+  id: "operation-nodemailer",
+  handler: async ({
+    from,
+    subject,
+    text,
+    url,
+    bcc,
+    SMTP_HOST,
+    SMTP_USER,
+    SMTP_PASS,
+    filename,
+  }) => {
+    const transporter = nodemailer.createTransport({
+      host: SMTP_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASS,
+      },
+    });
+
+    try {
+      const info = await transporter.sendMail({
+        from: from,
+        bcc: bcc,
+        subject: subject || "Nodemailer Test ✔",
+        text: text || "Hello world?", // nutzt übergebenen Text, falls vorhanden
+        html: `<b>${text || "Hello world?"}</b>`,
+        attachments: [
+          {
+            filename: filename || "foto.jpeg",
+            href:
+              url ||
+              "https://raw.githubusercontent.com/nodemailer/nodemailer/master/LICENSE",
+          },
+        ],
+      });
+
+      return {
+        success: true,
+        messageId: info.messageId,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
+  },
+});
--- src/app.js
+++ src/app.js
@@ -1,23 +1,121 @@
 export default {
-	id: 'custom',
-	name: 'Custom',
-	icon: 'box',
-	description: 'This is my custom operation!',
-	overview: ({ text }) => [
-		{
-			label: 'Text',
-			text: text,
-		},
-	],
-	options: [
-		{
-			field: 'text',
-			name: 'Text',
-			type: 'string',
-			meta: {
-				width: 'full',
-				interface: 'input',
-			},
-		},
-	],
+  id: "operation-nodemailer",
+  name: "Nodemailer",
+  icon: "box",
+  description: "Sending mails with nodemailer!",
+  overview: ({
+    from,
+    subject,
+    text,
+    url,
+    bcc,
+    SMTP_HOST,
+    SMTP_USER,
+    SMTP_PASS,
+    filename,
+  }) => [
+    {
+      label: "from",
+      text: from,
+    },
+    {
+      label: "subject",
+      text: subject,
+    },
+    {
+      label: "Text",
+      text: text,
+    },
+    {
+      label: "URL",
+      text: url,
+    },
+    {
+      label: "bcc",
+      text: bcc,
+    },
+  ],
+  options: [
+    {
+      field: "from",
+      name: "from",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "subject",
+      name: "subject",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "text",
+      name: "Text",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "url",
+      name: "URL",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "bcc",
+      name: "bcc",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "SMTP_HOST",
+      name: "SMTP_HOST",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "SMTP_USER",
+      name: "SMTP_USER",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "SMTP_PASS",
+      name: "SMTP_PASS",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+    {
+      field: "filename",
+      name: "filename",
+      type: "string",
+      meta: {
+        width: "full",
+        interface: "input",
+      },
+    },
+  ],
 };
